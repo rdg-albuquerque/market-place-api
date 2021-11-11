f4b3eb94a0bdcbb494aa60f2f1949e0934a3617a
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-11 17:54:31 -0300

    Refatoração do código / Agora o schema de cadastrar produto não recebe mais a imagem pois há um endpoint específico para o upload da imagem do produto

--- src/utils/validacoes/validacoes-produtos.js
+++ src/utils/validacoes/validacoes-produtos.js
@@ -1,8 +1,7 @@
-const yup = require('yup')
-const { pt } = require('yup-locales')
+const yup = require("yup");
+const { pt } = require("yup-locales");
 
-yup.setLocale(pt)
-
+yup.setLocale(pt);
 
 const cadastrarProdutoSchema = yup.object().shape({
     nome: yup.string().required().min(3),
@@ -10,25 +9,23 @@ const cadastrarProdutoSchema = yup.object().shape({
     preco: yup.number().strict().required(),
     descricao: yup.string().required(),
     categoria: yup.string(),
-    imagem: yup.string(),
-    nome_imagem: yup.string()
-})
+});
 
 const atualizarProdutoSchema = yup.object().shape({
     nome: yup.string().min(3),
     estoque: yup.number().strict().min(1),
     preco: yup.number().strict().min(0),
     descricao: yup.string(),
-    categoria: yup.string()
-})
+    categoria: yup.string(),
+});
 
 const atualizarImgProdutoSchema = yup.object().shape({
-    imagem: yup.string(),
-    nome_imagem: yup.string()
-})
+    imagem: yup.string().required(),
+    nome_imagem: yup.string().required(),
+});
 
 module.exports = {
     cadastrarProdutoSchema,
     atualizarProdutoSchema,
-    atualizarImgProdutoSchema
-}
+    atualizarImgProdutoSchema,
+};
