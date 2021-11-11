de9da622682541b83cc271e14879429354dcf18d
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-11 17:22:10 -0300

    criação de rotas de atualizar e deleção de imagens do produto

--- src/rotas.js
+++ src/rotas.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const usuarios = require('./controladores/usuarios');
-const login = require('./controladores/login');
-const produtos = require('./controladores/produtos');
-const verificaLogin = require('./filtros/verificaLogin');
+const express = require("express");
+const usuarios = require("./controladores/usuarios");
+const login = require("./controladores/login");
+const produtos = require("./controladores/produtos");
+const verificaLogin = require("./filtros/verificaLogin");
 
 const rotas = express();
 
 // cadastro de usuario
-rotas.post('/usuarios', usuarios.cadastrarUsuario);
+rotas.post("/usuarios", usuarios.cadastrarUsuario);
 
 // login
-rotas.post('/login', login.login);
+rotas.post("/login", login.login);
 
 // filtro para verificar usuario logado
 rotas.use(verificaLogin);
 
 // obter e atualizar perfil do usuario logado
-rotas.get('/perfil', usuarios.obterPerfil);
-rotas.put('/perfil', usuarios.atualizarPerfil);
+rotas.get("/perfil", usuarios.obterPerfil);
+rotas.put("/perfil", usuarios.atualizarPerfil);
 
 // crud de produtos
-rotas.get('/produtos', produtos.listarProdutos);
-rotas.get('/produtos/:id', produtos.obterProduto);
-rotas.post('/produtos', produtos.cadastrarProduto);
-rotas.put('/produtos/:id', produtos.atualizarProduto);
-rotas.delete('/produtos/:id', produtos.excluirProduto);
-rotas.put('/produtos/:id/img/att', produtos.atualizarImgProduto);
-rotas.put('/produtos/:id/img/excluir', produtos.excluirImgProduto);
+rotas.get("/produtos", produtos.listarProdutos);
+rotas.get("/produtos/:id", produtos.obterProduto);
+rotas.post("/produtos", produtos.cadastrarProduto);
+rotas.put("/produtos/:id", produtos.atualizarProduto);
+rotas.delete("/produtos/:id", produtos.excluirProduto);
+
+//crud de imagens dos produtos
+rotas.put("/produtos/:id/img", produtos.atualizarImgProduto);
+rotas.delete("/produtos/:id/img", produtos.excluirImgProduto);
 
 module.exports = rotas;
