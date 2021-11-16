2b18887fb630dc50faaf39a121cb904fb1a8c560
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-15 22:48:03 -0300

    mudança no nome do parâmetro da função

--- src/servicos/supabase/utils/upload.js
+++ src/servicos/supabase/utils/upload.js
@@ -1,8 +1,8 @@
 const supabase = require("../supabase");
 
-async function upload(nome_imagem, imagem) {
+async function upload(path, imagem) {
     const imgBuffer = Buffer.from(imagem, "base64");
-    const response = await supabase.storage.from(process.env.STORAGE_BUCKET).upload(nome_imagem, imgBuffer);
+    const response = await supabase.storage.from(process.env.STORAGE_BUCKET).upload(path, imgBuffer);
 
     return response;
 }
